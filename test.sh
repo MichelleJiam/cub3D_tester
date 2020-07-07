6cbc39b32c7ac45d83ee58181af98dd5cd07f22a
Michelle Jiam <mjiam@student.codam.nl>
2020-07-07 20:35:21 +0200

    Added extra invalid char checks for res and col

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@
 #    By: mjiam <mjiam@student.codam.nl>               +#+                      #
 #                                                    +#+                       #
 #    Created: 2020/06/11 14:44:51 by mjiam         #+#    #+#                  #
-#    Updated: 2020/06/19 14:39:02 by mjiam         ########   odam.nl          #
+#    Updated: 2020/07/07 20:33:43 by mjiam         ########   odam.nl          #
 #                                                                              #
 # **************************************************************************** #
 
@@ -24,13 +24,13 @@ if [ ! -f "../cub3D" ]; then
     printf "${GREEN}Error: ./cub3D not found. Run make first${RESET}\n"
 else
     printf "\n${BLUEBG}${WHITE}Beginning Invalid Map Test${RESET}\n"
-    printf "\n${CYAN}Test ${TNUM}/25: < 0 floor colour values${RESET}\n"
+    printf "\n${CYAN}Test ${TNUM}/27: < 0 floor colour values${RESET}\n"
     cd ..; ./cub3D cub3d_tester/maps/col_inv.cub
     ((TNUM+=1))
     printf "\n${BLUEBG}${WHITE}Press ENTER for next test${RESET}\n"
     read -s -n 1 KEY
     if [$KEY == ""] ; then
-        printf "\n${CYAN}Test ${TNUM}/25: > 255 ceiling colour value${RESET}\n"
+        printf "\n${CYAN}Test ${TNUM}/27: > 255 ceiling colour value${RESET}\n"
         ./cub3D cub3d_tester/maps/col_inv2.cub
         ((TNUM+=1))
         printf "\n${BLUEBG}${WHITE}Press ENTER for next test${RESET}\n"
@@ -40,7 +40,7 @@ else
     fi
     read -s -n 1 KEY
     if [$KEY == ""] ; then
-        printf "\n${CYAN}Test ${TNUM}/25: invalid character between colour value${RESET}\n"
+        printf "\n${CYAN}Test ${TNUM}/27: invalid character between colour value${RESET}\n"
         ./cub3D cub3d_tester/maps/col_inv3.cub
         ((TNUM+=1))
         printf "\n${BLUEBG}${WHITE}Press ENTER for next test${RESET}\n"
@@ -50,7 +50,7 @@ else
     fi
     read -s -n 1 KEY
     if [$KEY == ""] ; then
-        printf "\n${CYAN}Test ${TNUM}/25: missing 1 of ceiling colour values${RESET}\n"
+        printf "\n${CYAN}Test ${TNUM}/27: missing 1 of ceiling colour values${RESET}\n"
         ./cub3D cub3d_tester/maps/col_inv4.cub
         ((TNUM+=1))
         printf "\n${BLUEBG}${WHITE}Press ENTER for next test${RESET}\n"
@@ -60,7 +60,17 @@ else
     fi
     read -s -n 1 KEY
     if [$KEY == ""] ; then
-        printf "\n${CYAN}Test ${TNUM}/25: valid 0,0,0 floor colour${RESET}\n"
+        printf "\n${CYAN}Test ${TNUM}/27: invalid character between colour values${RESET}\n"
+        ./cub3D cub3d_tester/maps/col_inv5.cub
+        ((TNUM+=1))
+        printf "\n${BLUEBG}${WHITE}Press ENTER for next test${RESET}\n"
+    else
+        printf "Exiting test\n"
+        exit 1
+    fi
+    read -s -n 1 KEY
+    if [$KEY == ""] ; then
+        printf "\n${CYAN}Test ${TNUM}/27: valid 0,0,0 floor colour${RESET}\n"
         ./cub3D cub3d_tester/maps/col_valid0.cub
         ((TNUM+=1))
         printf "\n${BLUEBG}${WHITE}Press ENTER for next test${RESET}\n"
@@ -70,7 +80,7 @@ else
     fi
     read -s -n 1 KEY
     if [$KEY == ""] ; then
-        printf "\n${PURP}Test ${TNUM}/25: duplicate starting direction${RESET}\n"
+        printf "\n${PURP}Test ${TNUM}/27: duplicate starting direction${RESET}\n"
         ./cub3D cub3d_tester/maps/dir_dup.cub
         ((TNUM+=1))
         printf "\n${BLUEBG}${WHITE}Press ENTER for next test${RESET}\n"
@@ -80,7 +90,7 @@ else
     fi
     read -s -n 1 KEY
     if [$KEY == ""] ; then
-        printf "\n${PURP}Test ${TNUM}/25: invalid starting direction character${RESET}\n"
+        printf "\n${PURP}Test ${TNUM}/27: invalid starting direction character${RESET}\n"
         ./cub3D cub3d_tester/maps/dir_inv.cub
         ((TNUM+=1))
         printf "\n${BLUEBG}${WHITE}Press ENTER for next test${RESET}\n"
@@ -90,7 +100,7 @@ else
     fi
     read -s -n 1 KEY
     if [$KEY == ""] ; then
-        printf "\n${PURP}Test ${TNUM}/25: missing starting direction${RESET}\n"
+        printf "\n${PURP}Test ${TNUM}/27: missing starting direction${RESET}\n"
         ./cub3D cub3d_tester/maps/dir_none.cub
         ((TNUM+=1))
         printf "\n${BLUEBG}${WHITE}Press ENTER for next test${RESET}\n"
@@ -100,7 +110,7 @@ else
     fi
     read -s -n 1 KEY
     if [$KEY == ""] ; then
-        printf "\n${BLUE}Test ${TNUM}/25: duplicate wall texture${RESET}\n"
+        printf "\n${BLUE}Test ${TNUM}/27: duplicate wall texture${RESET}\n"
         ./cub3D cub3d_tester/maps/tex_dup.cub
         ((TNUM+=1))
         printf "\n${BLUEBG}${WHITE}Press ENTER for next test${RESET}\n"
@@ -110,7 +120,7 @@ else
     fi
     read -s -n 1 KEY
     if [$KEY == ""] ; then
-        printf "\n${BLUE}Test ${TNUM}/25: invalid element identifier${RESET}\n"
+        printf "\n${BLUE}Test ${TNUM}/27: invalid element identifier${RESET}\n"
         ./cub3D cub3d_tester/maps/tex_inv.cub
         ((TNUM+=1))
         printf "\n${BLUEBG}${WHITE}Press ENTER for next test${RESET}\n"
@@ -120,7 +130,7 @@ else
     fi
     read -s -n 1 KEY
     if [$KEY == ""] ; then
-        printf "\n${BLUE}Test ${TNUM}/25: invalid sprite texture file${RESET}\n"
+        printf "\n${BLUE}Test ${TNUM}/27: invalid sprite texture file${RESET}\n"
         ./cub3D cub3d_tester/maps/tex_invfile.cub
         ((TNUM+=1))
         printf "\n${BLUEBG}${WHITE}Press ENTER for next test${RESET}\n"
@@ -130,7 +140,7 @@ else
     fi
     read -s -n 1 KEY
     if [$KEY == ""] ; then
-        printf "\n${BLUE}Test ${TNUM}/25: missing East wall texture${RESET}\n"
+        printf "\n${BLUE}Test ${TNUM}/27: missing East wall texture${RESET}\n"
         ./cub3D cub3d_tester/maps/tex_miss.cub
         ((TNUM+=1))
         printf "\n${BLUEBG}${WHITE}Press ENTER for next test${RESET}\n"
@@ -140,7 +150,7 @@ else
     fi
     read -s -n 1 KEY
     if [$KEY == ""] ; then
-        printf "\n${BLUE}Test ${TNUM}/25: space in texture file path. Should run correctly.${RESET}\n"
+        printf "\n${BLUE}Test ${TNUM}/27: space in texture file path. Should run correctly.${RESET}\n"
         ./cub3D cub3d_tester/maps/tex_space.cub
         ((TNUM+=1))
         printf "\n${BLUEBG}${WHITE}Press ENTER for next test${RESET}\n"
@@ -150,7 +160,7 @@ else
     fi
     read -s -n 1 KEY
     if [$KEY == ""] ; then
-        printf "\n${CYAN}Test ${TNUM}/25: newline in map${RESET}\n"
+        printf "\n${CYAN}Test ${TNUM}/27: newline in map${RESET}\n"
         ./cub3D cub3d_tester/maps/map_nl.cub
         ((TNUM+=1))
         printf "\n${BLUEBG}${WHITE}Press ENTER for next test${RESET}\n"
@@ -160,7 +170,7 @@ else
     fi
     read -s -n 1 KEY
     if [$KEY == ""] ; then
-        printf "\n${CYAN}Test ${TNUM}/25: missing map${RESET}\n"
+        printf "\n${CYAN}Test ${TNUM}/27: missing map${RESET}\n"
         ./cub3D cub3d_tester/maps/map_none.cub
         ((TNUM+=1))
         printf "\n${BLUEBG}${WHITE}Press ENTER for next test${RESET}\n"
@@ -170,7 +180,7 @@ else
     fi
     read -s -n 1 KEY
     if [$KEY == ""] ; then
-        printf "\n${CYAN}Test ${TNUM}/25: invalid open map${RESET}\n"
+        printf "\n${CYAN}Test ${TNUM}/27: invalid open map${RESET}\n"
         ./cub3D cub3d_tester/maps/map_open.cub
         ((TNUM+=1))
         printf "\n${BLUEBG}${WHITE}Press ENTER for next test${RESET}\n"
@@ -180,7 +190,7 @@ else
     fi
     read -s -n 1 KEY
     if [$KEY == ""] ; then
-        printf "\n${CYAN}Test ${TNUM}/25: invalid open map #2 (sprite)${RESET}\n"
+        printf "\n${CYAN}Test ${TNUM}/27: invalid open map #2 (sprite)${RESET}\n"
         ./cub3D cub3d_tester/maps/map_opensp.cub
         ((TNUM+=1))
         printf "\n${BLUEBG}${WHITE}Press ENTER for next test${RESET}\n"
@@ -190,7 +200,7 @@ else
     fi
     read -s -n 1 KEY
     if [$KEY == ""] ; then
-        printf "\n${CYAN}Test ${TNUM}/25: invalid open map #3 (start in wall)${RESET}\n"
+        printf "\n${CYAN}Test ${TNUM}/27: invalid open map #3 (start in wall)${RESET}\n"
         ./cub3D cub3d_tester/maps/map_opendir.cub
         ((TNUM+=1))
         printf "\n${BLUEBG}${WHITE}Press ENTER for next test${RESET}\n"
@@ -200,7 +210,7 @@ else
     fi
     read -s -n 1 KEY
     if [$KEY == ""] ; then
-        printf "\n${CYAN}Test ${TNUM}/25: invalid open map #4 (diagonal)${RESET}\n"
+        printf "\n${CYAN}Test ${TNUM}/27: invalid open map #4 (diagonal)${RESET}\n"
         ./cub3D cub3d_tester/maps/map_opendiag.cub
         ((TNUM+=1))
         printf "\n${BLUEBG}${WHITE}Press ENTER for next test${RESET}\n"
@@ -210,7 +220,7 @@ else
     fi
     read -s -n 1 KEY
     if [$KEY == ""] ; then
-        printf "\n${CYAN}Test ${TNUM}/25: map full of sprites. Should render correctly.${RESET}\n"
+        printf "\n${CYAN}Test ${TNUM}/27: map full of sprites. Should render correctly.${RESET}\n"
         ./cub3D cub3d_tester/maps/map_sprite.cub
         ((TNUM+=1))
         printf "\n${BLUEBG}${WHITE}Press ENTER for next test${RESET}\n"
@@ -220,7 +230,7 @@ else
     fi
     read -s -n 1 KEY
     if [$KEY == ""] ; then
-        printf "\n${CYAN}Test ${TNUM}/25: valid open map. Should run correctly.${RESET}\n"
+        printf "\n${CYAN}Test ${TNUM}/27: valid open map. Should run correctly.${RESET}\n"
         ./cub3D cub3d_tester/maps/map_openvalid.cub
         ((TNUM+=1))
         printf "\n${BLUEBG}${WHITE}Press ENTER for next test${RESET}\n"
@@ -230,7 +240,7 @@ else
     fi
     read -s -n 1 KEY
     if [$KEY == ""] ; then
-        printf "\n${PURP}Test ${TNUM}/25: int overflow resolution. Should resize and run correctly.${RESET}\n"
+        printf "\n${PURP}Test ${TNUM}/27: int overflow resolution. Should resize and run correctly.${RESET}\n"
         ./cub3D cub3d_tester/maps/res_overfl.cub
         ((TNUM+=1))
         printf "\n${BLUEBG}${WHITE}Press ENTER for next test${RESET}\n"
@@ -240,7 +250,7 @@ else
     fi
     read -s -n 1 KEY
     if [$KEY == ""] ; then
-        printf "\n${PURP}Test ${TNUM}/25: 3600x2800 resolution. Should resize and run correctly.${RESET}\n"
+        printf "\n${PURP}Test ${TNUM}/27: 3600x2800 resolution. Should resize and run correctly.${RESET}\n"
         ./cub3D cub3d_tester/maps/res_big.cub
         ((TNUM+=1))
         printf "\n${BLUEBG}${WHITE}Press ENTER for next test${RESET}\n"
@@ -250,7 +260,7 @@ else
     fi
     read -s -n 1 KEY
     if [$KEY == ""] ; then
-        printf "\n${PURP}Test ${TNUM}/25: 3600x2800 resolution with --SAVE. Should not resize.${RESET}\n"
+        printf "\n${PURP}Test ${TNUM}/27: 3600x2800 resolution with --SAVE. Should not resize.${RESET}\n"
         ./cub3D cub3d_tester/maps/res_big.cub --save
         ((TNUM+=1))
         BMP=$(ls -t *.bmp | head -1)
@@ -267,7 +277,17 @@ else
     fi
     read -s -n 1 KEY
     if [$KEY == ""] ; then
-    printf "\n${PURP}Test ${TNUM}/25: < 0 resolution. Should error.${RESET}\n"
+        printf "\n${CYAN}Test ${TNUM}/27: invalid character in resolution. Should error.${RESET}\n"
+        ./cub3D cub3d_tester/maps/res_inv2.cub
+        ((TNUM+=1))
+        printf "\n${BLUEBG}${WHITE}Press ENTER for next test${RESET}\n"
+    else
+        printf "Exiting test\n"
+        exit 1
+    fi
+    read -s -n 1 KEY
+    if [$KEY == ""] ; then
+    printf "\n${PURP}Test ${TNUM}/27: < 0 resolution. Should error.${RESET}\n"
     ./cub3D cub3d_tester/maps/res_inv.cub
     ((TNUM+=1))
     fi
